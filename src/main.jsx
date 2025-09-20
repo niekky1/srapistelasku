27bd940c078832bc3becba0e5f10144448f59341
niekky1 <ville.niemi1@gmail.com>
2025-09-20 18:52:49 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'                 // ← tämä mukaan
 import SraPistesivu from './components/SraPistesivu'
+
 ReactDOM.createRoot(document.getElementById('root')).render(<SraPistesivu />)
