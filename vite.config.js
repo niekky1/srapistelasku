6e40ea71b612e27b4db2b8da57ebca446bc22e16
niekky1 <ville.niemi1@gmail.com>
2025-09-20 18:12:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   base: '/',
   build: {
     outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // isot kirjastot omiin palasiin
+          html2canvas: ['html2canvas'],
+          dompurify: ['dompurify'],
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
   },
 })
